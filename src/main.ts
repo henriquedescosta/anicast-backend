958e3b1c2d5624fa15030a11fbf72e7759aaf095
Henrique <henriquedescosta@gmail.com>
2022-06-05 19:11:20 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     )
     .setVersion('1.0')
     .build();
-
+  app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
